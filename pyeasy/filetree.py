82dc412e1f90f10ba24e1054b37633f3a6b46eb4
twz915 <twz915@qq.com>
2014-03-31 12:57:20 +0800

    add print file tree script

---
+++ pyeasy/filetree.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+import os
+
+def fileTree(path, count=0):
+    if not os.path.exists(path):
+        return
+
+    if os.path.isfile(path):
+        fileName = os.path.basename(path)
+        print '    ' * count + '|-- ' + fileName 
+    elif os.path.isdir(path):
+        print '    ' * count + '|-- ' + path 
+        pathList = os.listdir(path)
+        for eachPath in pathList:
+            fileTree(os.path.join(path,eachPath),count+1)
+ 
+if __name__ == '__main__':
+	fileTree('G:\python\pyeasy')
