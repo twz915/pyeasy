92be7a34d20e9076dc2718d13e0bdb132c53d486
twz915 <twz915@qq.com>
2014-03-31 13:01:39 +0800

    find package automatically

--- setup.py
+++ setup.py
@@ -1,17 +1,11 @@
-
-#  Copyright (C) 2013 pyeasy
+#  Copyright (C) 2014 pyeasy
 #  All rights reserved.
-
-from distutils.core import setup
-
+from setuptools import setup, find_packages
 
 setup(
 	name = 'pyeasy',
 	version = '1.0.0',
 	author = 'tuweizhong',
-	author_email = 'tuweizhong@163.com',   
-
-	packages = [
-	'pyeasy',
-	],
-      )
+	author_email = 'tuweizhong@163.com',
+	packages = find_packages(),
+ )
